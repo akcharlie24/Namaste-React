490fbedb88ac41af9ecc9746bd54893f3ec6650a
akcharlie24 <akshatbhardwaj24@gmail.com>
2024-03-02 15:28:53 +0530

    changes made

---
+++ Food-villa/src/components/Title.js
@@ -0,0 +1,9 @@
+const title = (
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://lh3.gkoogleusercontent.com/p/AF1QipO_6cTc3QdC9L2vAOyCkUPG-G-9YeFxo3YiDu3R=w1080-h608-p-no-v0"
+      ></img>
+    </a>
+  );
