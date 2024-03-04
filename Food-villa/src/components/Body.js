39a9da5cc46759f1f62b6b655dd46f0debf4e698
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-03-04 12:55:23 +0530

    changes made

--- Food-villa/src/components/Body.js
+++ Food-villa/src/components/Body.js
@@ -3,20 +3,36 @@ import { restaurantList } from "../config";
 import { useState } from "react";
 
 const RestaurantCard = (props) => {
-  const { name, cloudinaryImageId, areaName, cuisines } = props;
+  const { name, cloudinaryImageId, areaName, cuisines, avgRating } = props;
   return (
     <div className="w-64 border-2 bg-gray-200 m-2 flex flex-col justify-center items-center rounded ">
       <img
         src={IMG_CDN_LINK + cloudinaryImageId}
         className="w-56 m-1 border border-black rounded-md"
       ></img>
-      <p className="font-bold">{name}</p>
+      <p className="font-bold text-center">
+        {name} ({avgRating} stars)
+      </p>
+      <p className="mt-2">{areaName}</p>
+      <p className="mt-2 w-64 h-auto overflow-x-hidden overflow-y-hidden text-ellipsis text-center">
+        {cuisines.join(",")}
+      </p>
     </div>
   );
 };
 
 const Body = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const toggleSearch = () => {
+    const restaurants = restaurantList.filter((r) =>
+      r.info.name
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\[\]"'\\?]/g, "")
+        .toLowerCase()
+        .includes(searchValue.toLowerCase())
+    );
+    setRestaurants(restaurants);
+  };
   // const searchValue = "kfc"; this doesnt work in react
   return (
     <>
@@ -26,12 +42,18 @@ const Body = () => {
           type="text"
           placholder="search"
           value={searchValue}
-          onChange={(e) => (setSearchValue(e.target.value))}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onSubmit={toggleSearch}
         />
-        <button className="search-btn bg-gray-400 border-2 w-1/12 h-8 border-black rounded mx-4 ">Search</button>
+        <button
+          className="search-btn bg-gray-400 border-2 w-1/12 h-8 border-black rounded mx-4 "
+          onClick={toggleSearch}
+        >
+          Search
+        </button>
       </div>
       <div className="flex flex-wrap justify-center ">
-        {restaurantList.map((r) => (
+        {restaurants.map((r) => (
           <RestaurantCard key={r.info.id} {...r.info} />
         ))}
       </div>
